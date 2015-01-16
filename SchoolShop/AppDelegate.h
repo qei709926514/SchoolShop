43768dd8a62e9a8ff4b7176bee30a1d311b67c57
qei709926514 <qei@dudududdedeiMac.lan>
2015-01-16 21:43:29 +0800

    Initial Commit

---
+++ SchoolShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolShop
+//
+//  Created by 度度度度的 on 15/1/16.
+//  Copyright (c) 2015年 度度度度的. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolShop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SchoolShop
+//
+//  Created by 度度度度的 on 15/1/16.
+//  Copyright (c) 2015年 度度度度的. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
